8deedb9c61a4d6e9b10145b5f9a18e9d5ac7a595
Jurandi Junior <jurandijr20@gmail.com>
2023-01-31 18:56:20 -0300

    Tipo inteiro: int

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaVariaveis.java
@@ -0,0 +1,23 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste");
+		
+		int idade;
+		idade = 22;
+		
+		System.out.println(idade);
+		
+		idade = 22 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) + 2;
+		
+		System.out.println(idade);
+		
+		System.out.println("A idade é " + idade);
+	}
+}
