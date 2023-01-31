178796470638668ecc6d475f6bd6ec3381ee6c8b
Jurandi Junior <jurandijr20@gmail.com>
2023-01-31 19:31:35 -0300

    Declarando String e char

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaCaracteres.java
@@ -0,0 +1,22 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char)(valor + 1);
+		System.out.println(valor);
+		
+		String frase = "Alura cursos online de tecnologia";
+		System.out.println(frase);
+		
+		frase = frase + " " + 2023;
+		System.out.println(frase);
+	}
+
+}
