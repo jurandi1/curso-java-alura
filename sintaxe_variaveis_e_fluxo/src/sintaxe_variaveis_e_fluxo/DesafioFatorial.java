43ae92844c40c6d5a296c3592bd8cef82d889e00
Jurandi Junior <jurandijr20@gmail.com>
2023-01-31 21:48:36 -0300

    Desafio: Fatorial

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/DesafioFatorial.java
@@ -0,0 +1,15 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class DesafioFatorial {
+
+	public static void main(String[] args) {
+		
+		int fatorial = 1;
+		
+        for (int i = 1; i < 11; i++) {
+            fatorial *= i;
+            System.out.println("O fatorial de " + i + " = " + fatorial);
+        }
+	}
+
+}
