0fe2c50a1f863fbb5443e52718f2c0d62a3f0e9a
Jurandi Junior <jurandijr20@gmail.com>
2023-01-31 23:05:05 -0300

    Referências vs Objetos

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -10,18 +10,29 @@ public class CriaConta {
 		System.out.println(primeiraConta.saldo);
 		
 		Conta segundaConta = new Conta();
-		segundaConta.saldo = 50;
+		segundaConta.saldo = 300;
 		
 		System.out.println("A primeira conta tem " + primeiraConta.saldo);
 		System.out.println("A segunda conta tem " + segundaConta.saldo);
 		
+		primeiraConta.agencia= 146;
 		System.out.println(primeiraConta.agencia);
 		System.out.println(primeiraConta.numero);
 		
 		System.out.println(segundaConta.agencia);
 		
 		segundaConta.agencia= 146;
-		System.out.println("Agora a segunda conta está na agencia " + segundaConta.agencia);
+		System.out.println("Agora a segunda conta está na agencia "
+		+ segundaConta.agencia);
+		
+		if(primeiraConta ==  segundaConta) {
+			System.out.println("São a mesma conta");
+		} else {
+			System.out.println("Contas diferentes");
+		}
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
 		
 	}
 
