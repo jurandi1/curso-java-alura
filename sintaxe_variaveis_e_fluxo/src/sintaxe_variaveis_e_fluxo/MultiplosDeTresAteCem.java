4775d9acbb0b91f12239c47ce9dce62e88495b12
Jurandi Junior <jurandijr20@gmail.com>
2023-01-31 21:42:41 -0300

    Desafio: Múltiplos de 3

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/MultiplosDeTresAteCem.java
@@ -0,0 +1,12 @@
+package sintaxe_variaveis_e_fluxo;
+
+class MultiplosDeTresAteCem {
+	
+    public static void main (String[] args) {
+    	
+        for (int multiplicador = 3; multiplicador < 100; multiplicador += 3 ){
+            System.out.println(multiplicador);
+        }
+    }
+    
+}
