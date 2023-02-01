2afac51a73ce5809a9ef6d7ef2f91b64beff3765
Jurandi Junior <jurandijr20@gmail.com>
2023-02-01 19:16:29 -0300

    Métodos com referência e mais retorno

--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -2,13 +2,30 @@
 public class TestaMetodo {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
-		System.out.println(conta.saldo);
-		boolean conseguiuRetirar = conta.saca(20);
-		conta.saca(20);
-		System.out.println(conta.saldo);
+		Conta contaDoJunior = new Conta();
+		contaDoJunior.saldo = 100;
+		contaDoJunior.deposita(50);
+		System.out.println(contaDoJunior.saldo);
+		
+		boolean conseguiuRetirar = contaDoJunior.saca(20);
+		contaDoJunior.saca(20);
+		System.out.println(contaDoJunior.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaAmanda = new Conta();
+		contaDaAmanda.deposita(1000);
+		
+		boolean sucessoTransferencia = 
+				contaDaAmanda.transfere(3000, contaDoJunior);
+		
+		if(sucessoTransferencia) {
+			System.out.println("Transferência feita com sucesso!");
+		} else {
+			System.out.println("Faltou dinheiro.");
+		}
+		
+		System.out.println(contaDaAmanda.saldo);
+		System.out.println(contaDoJunior.saldo);
 	}
 
 }
