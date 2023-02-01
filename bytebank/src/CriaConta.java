ca5417844f25c50eafd05a9a9400117e37e46bbe
Jurandi Junior <jurandijr20@gmail.com>
2023-01-31 22:49:03 -0300

    Valores default de atributos

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -15,6 +15,14 @@ public class CriaConta {
 		System.out.println("A primeira conta tem " + primeiraConta.saldo);
 		System.out.println("A segunda conta tem " + segundaConta.saldo);
 		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		System.out.println(segundaConta.agencia);
+		
+		segundaConta.agencia= 146;
+		System.out.println("Agora a segunda conta está na agencia " + segundaConta.agencia);
+		
 	}
 
 }
