599f5f0e453c5f2e584d607eec04d0f2ad7b7198
Jurandi Junior <jurandijr20@gmail.com>
2023-02-01 21:59:51 -0300

    Getters e Setters de referência

--- bytebank_encapsulado/src/Conta.java
+++ bytebank_encapsulado/src/Conta.java
@@ -2,7 +2,7 @@ public class Conta {
 	private double saldo;
 	private int agencia;
 	private int numero;
-	Cliente titular;
+	private Cliente titular;
 	
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -45,6 +45,14 @@ public class Conta {
 	
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
 	}
 	
 }
