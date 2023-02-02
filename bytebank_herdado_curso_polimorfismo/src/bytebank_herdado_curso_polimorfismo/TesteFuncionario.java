920d4c8d087a77bf59d01e7185362f032b119436
Jurandi Junior <jurandijr20@gmail.com>
2023-02-02 19:52:31 -0300

    Inicindo o curso de Polimorfismo - Revisão e a classe Funcionario

---
+++ bytebank_herdado_curso_polimorfismo/src/bytebank_herdado_curso_polimorfismo/TesteFuncionario.java
@@ -0,0 +1,17 @@
+package bytebank_herdado_curso_polimorfismo;
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario junior = new Funcionario();
+		junior.setNome("Jurandi Junior");
+		junior.setCpf("111.111.111-04");
+		junior.setSalario(2600.00);
+		
+		System.out.println(junior.getNome());
+		System.out.println(junior.getBonificacao());
+
+	}
+
+}
