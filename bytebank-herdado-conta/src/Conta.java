d769c40a6a0e34fe9b47f3648d8c69291260af5c
Jurandi Junior <jurandijr20@gmail.com>
2023-02-06 19:15:58 -0300

    Herança e construtores

---
+++ bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,80 @@
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	//static faz com que a declaração vire da class: é um atributo da class
+	private static int total;
+	
+	//Contrutor padrão
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("o total de contas é " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("Criando conta " + this.numero);
+	}
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor ) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+		
+	public boolean transfere(double valor, Conta destino) {
+			if(this.saldo >= valor) {
+				this.saldo -= valor;
+				destino.deposita(valor);
+				return true;
+			} else {
+				return false;
+			}
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Não pode valor menor ou igual a 0.");
+			return;
+		}
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			System.out.println("Não pode valor igual a 0.");
+			return;
+		}
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
+	}
+}
