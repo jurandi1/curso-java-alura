f0c32557c4db0eca1491a3a2fef3809f701d0900
Jurandi Junior <jurandijr20@gmail.com>
2023-02-08 21:50:48 -0300

    Abstract no exemplo Conta

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -1,5 +1,5 @@
-public class Conta {
-	private double saldo;
+public abstract class Conta {
+	protected double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
@@ -16,9 +16,7 @@ public class Conta {
 //		System.out.println("Criando conta " + this.numero);
 	}
 	
-	public void deposita(double valor) {
-		this.saldo += valor;
-	}
+	public abstract void deposita(double valor);
 	
 	public boolean saca(double valor ) {
 		if(this.saldo >= valor) {
