2bb989efb7fa4baca06bcf185a6f903d5e043271
Jurandi Junior <jurandijr20@gmail.com>
2023-02-12 18:09:10 -0300

    Importando pacotes - nomeando o package com boa prática

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteSaca.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteSaca.java
@@ -1,15 +1,17 @@
-package teste;
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.*;
 
 public class TesteSaca {
 	
 	public static void main(String[] args) {
-		modelo.Conta conta = new modelo.ContaCorrente(123, 321);
+		Conta conta = new ContaCorrente(123, 321);
 		
 		conta.deposita(200.0);
 		
 		try {
 			conta.saca(210.0);
-		} catch(modelo.SaldoInsulficienteException e) {
+		} catch(SaldoInsulficienteException e) {
 			System.out.println("Exception: " + e.getMessage());
 		}
 		
