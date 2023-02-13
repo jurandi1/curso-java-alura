5fb52c13dd95b79206fab2107e0334668173c666
Jurandi Junior <jurandijr20@gmail.com>
2023-02-13 20:21:15 -0300

    Conhecendo java.lang

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
@@ -4,8 +4,10 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsulficienteException;
 
+//br.com.bytebank.banco.test.TesteContas
 public class TesteContas {
 
+	// java.lang.String
 	public static void main(String[] args) throws SaldoInsulficienteException {
 		//Full Qualifield name FQN
 		ContaCorrente cc =  new ContaCorrente(111, 111);
