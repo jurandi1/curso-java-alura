4169f09f5b2fa6d387463918cb1490de7468139e
Jurandi Junior <jurandijr20@gmail.com>
2023-02-17 19:26:08 -0300

    Começando com Arrays

---
+++ bytebank-herdado-conta/src/Teste.java
@@ -0,0 +1,30 @@
+public class Teste {
+	
+	//Array
+	public static void main(String[] args) {
+
+		int[] idades =  new int[5]; // Inicializa os arrays com 0
+
+		for(int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+		}
+		 
+		
+		for(int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+		
+//		idades[0] = 29;
+//		idades[1] = 39;
+//		idades[2] = 49;
+//		idades[3] = 59;
+//		idades[4] = 69;
+//			 
+//		int idade4 = idades[4];
+//		
+//		System.out.println(idade4);		
+//		System.out.println(idades.length);
+
+	}
+
+}
