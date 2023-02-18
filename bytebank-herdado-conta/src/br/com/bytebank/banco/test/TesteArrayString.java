3838791184ce063865045d5ef7b6e7c94b25297c
Jurandi Junior <jurandijr20@gmail.com>
2023-02-17 21:29:24 -0300

    Entendendo o array String args

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayString.java
@@ -0,0 +1,18 @@
+package br.com.bytebank.banco.test;
+
+public class TesteArrayString {
+
+	public static void main(String[] args) {
+		/*usamos o terminal do computador para passar
+		 * parametros a String da classe salvar e 
+		 * imprimir o resultado 
+		 * */
+		System.out.println("Funcionou!");
+		
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+		
+	}
+
+}
