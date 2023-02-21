24fc9e2fb35ce2762b6936bb4d26d930602eb033
Jurandi Junior <jurandijr20@gmail.com>
2023-02-21 19:50:05 -0300

    Finalmente Lambdas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -43,29 +43,17 @@ public class Teste {
 		lista.add(cc3);
 		lista.add(cc4);
 
-		lista.sort(new Comparator<Conta>() { //classe anonima
+		lista.sort((c1, c2) -> Integer.compare(c1.getNumero(), c2.getNumero()));
 
-				@Override
-				public int compare(Conta c1, Conta c2) {
-					
-					return Integer.compare(c1.getNumero(), c2.getNumero());
-				}
-			}
-		);
-		
-		Comparator<Conta> comp =  new Comparator<Conta>() { //classe anonima
-
-			@Override
-			public int compare(Conta c1, Conta c2) {
-				String nomeC1 = c1.getTitular().getNome();
-				String nomeC2 = c2.getTitular().getNome();
-				return nomeC1.compareTo(nomeC2);
-			}
+		Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+			String nomeC1 = c1.getTitular().getNome();
+			String nomeC2 = c2.getTitular().getNome();
+			return nomeC1.compareTo(nomeC2);
 		};
 		
-		for (Conta conta : lista) {
-			System.out.println(conta + ", " + conta.getTitular().getNome());
-		}
+		lista.sort(comp);
+
+		lista.forEach((conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()));
 
 	}
 
