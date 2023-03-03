47f438fab116c7107f0960c750f24543c6c0ffdc
Jurandi Junior <jurandijr20@gmail.com>
2023-03-03 19:21:30 -0300

    Parseando arquivo

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -1,6 +1,7 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -10,6 +11,23 @@ public class TesteLeitura2 {
 		while(sc.hasNextLine()) {
 			String linha = sc.nextLine();
 			System.out.println(linha);
+			
+			Scanner linhaSc = new Scanner(linha); 	
+			linhaSc.useDelimiter(",");
+			linhaSc.useLocale(Locale.US);
+			
+			String  valor1 = linhaSc.next();
+			int  valor2 = linhaSc.nextInt();
+			int  valor3 = linhaSc.nextInt();
+			String  valor4 = linhaSc.next();
+			double valor5 = linhaSc.nextDouble();
+			
+			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+			
+			linhaSc.close();
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
 		}
 		sc.close();
 	}
