b0c4eae698762e9ed1fcdfed397dee9534d90416
Jurandi Junior <jurandijr20@gmail.com>
2023-03-03 18:57:55 -0300

    Leitura com Scanner

---
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,17 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(new File("contas.csv"));
+		while(sc.hasNextLine()) {
+			String linha = sc.nextLine();
+			System.out.println(linha);
+		}
+		sc.close();
+	}
+
+}
