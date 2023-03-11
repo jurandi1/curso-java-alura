e94456791b66c0ade040d19d4e47fae2db85f3a3
Jurandi Junior <jurandijr20@gmail.com>
2023-03-11 10:48:58 -0300

    Outros sets e itarators

--- gerenciador_de_cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador_de_cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -19,9 +22,19 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a3);
 
 		System.out.println("Todos os alunos matriculados: ");
-		javaColecoes.getAlunos().forEach(a -> {
-			System.out.println(a);
-		});
+		
+		Set<Aluno> alunos = javaColecoes.getAlunos();
+		Iterator<Aluno> iterador = alunos.iterator();
+		while(iterador.hasNext()) {
+			Aluno proximo = iterador.next();
+			System.out.println(proximo);
+		}
+		
+//		Vector<Aluno> vector = new Vector<>();
+		
+//		for (Aluno a : javaColecoes.getAlunos()) {
+//			System.out.println(a);
+//		}
 		
 		System.out.println("O aluno " + a1 + " está matrículado?");
 		System.out.println(javaColecoes.estaMatriculado(a1));
