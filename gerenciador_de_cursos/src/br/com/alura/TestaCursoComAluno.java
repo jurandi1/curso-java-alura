2ace1f3f55fadaa89784d26886d7d4e70d0aaf25
Jurandi Junior <jurandijr20@gmail.com>
2023-03-11 09:34:50 -0300

    Equals e hashcode

--- gerenciador_de_cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador_de_cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -13,15 +13,30 @@ public class TestaCursoComAluno {
 		Aluno a1 = new Aluno("Jurandi Junior", 12345);
 		Aluno a2 = new Aluno("Amanda Oliveira", 54321);
 		Aluno a3 = new Aluno("Glauciane Nobre", 54321);
-		
+
 		javaColecoes.matricula(a1);
 		javaColecoes.matricula(a2);
 		javaColecoes.matricula(a3);
-		
+
 		System.out.println("Todos os alunos matriculados: ");
 		javaColecoes.getAlunos().forEach(a -> {
 			System.out.println(a);
 		});
+		
+		System.out.println("O aluno " + a1 + " está matrículado?");
+		System.out.println(javaColecoes.estaMatriculado(a1));
+		
+		Aluno junior =  new Aluno("Jurandi Junior", 12345);
+		System.out.println("E esse Junior, está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(junior));
+		
+		
+		System.out.println("O aluno a1 é equals ao Junior?");
+		System.out.println(a1.equals(junior));
+		
+		//obrigatoriamente o seguinte é true:
+		System.out.println("Testando o hashCode: ");
+		System.out.println(a1.hashCode() == junior.hashCode());
 		
 	}
 
